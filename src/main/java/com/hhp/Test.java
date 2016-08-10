0acecc18565c5104d2d890b7431ecdcb2fa119e1
huanghaopeng <huanghaopeng@u51.com>
2016-08-10 16:57:47 +0800

    git init

---
+++ src/main/java/com/hhp/Test.java
@@ -0,0 +1,10 @@
+package com.hhp;
+
+/**
+ * Created by huanghaopeng on 16/8/10.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.print("dasfasf");
+    }
+}
