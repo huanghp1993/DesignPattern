4e51ec3e38b06522f141030fe074a99179833d6c
huanghaopeng <huanghaopeng@u51.com>
2017-02-08 15:18:34 +0800

    lambda表达式学习

--- src/main/java/com/hhp/tool/CalendarDemo.java
+++ src/main/java/com/hhp/tool/CalendarDemo.java
@@ -1,10 +1,16 @@
 package com.hhp.tool;
 
+import java.util.Calendar;
+
 /**
+ * 获取当前时间的小时
  * Created by huanghaopeng on 17/2/8.
  */
 public class CalendarDemo {
     public static void main(String[] args) {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR);
 
+        System.out.print(hour);
     }
 }
